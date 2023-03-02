6a3d87bba7d7c0fff0aee2bd72e21dffba48b1b3
Amin Rouholamini <nomad.cr34@gmail.com>
2023-03-02 15:24:52 +0330

    Created Project

---
+++ src/main/java/ir/amin/textcompare/TextCompareApplication.java
@@ -0,0 +1,13 @@
+package ir.amin.textcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextCompareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/amin/textcompare/TextCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.amin.textcompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextCompareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
